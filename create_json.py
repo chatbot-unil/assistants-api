9fb442974d253da3a99ca472c608db673239410b
Denis Gremaud <denis.gremaud@gmail.com>
2023-11-20 20:52:09 +0100

    update

--- create_json.py
+++ create_json.py
@@ -1,29 +1,23 @@
 import json
-import sys
 import os
 import argparse
+import sys
 
 parser = argparse.ArgumentParser(description='Convert CSV files to JSON.')
 parser.add_argument('--path', default='data/csv', help='Path to the CSV file or directory containing CSV files')
+parser.add_argument('--output_name', default='students_data.json', help='Name of the output JSON file')
 args = parser.parse_args()
 
 def save_to_json(data, filiere):
-    # Split the input data into lines
     lines = data.strip().split('\n')
-    
-    # Parse the header
     header = lines[0].split('; ')
-    
-    # Initialize the result dictionary
-    conexte = f"Ce document retrace les statistiques du nombres d'étudiant(nationalité, sexe, nationalité) inscrit au semestre d'automne en {filiere} depuis 2012 a l'université de Lausanne."
-    result = {"contexte": conexte}
-
-    # Process each data line
+    result = {}
     for line in lines[1:]:
         values = line.split('; ')
         year_data = dict(zip(header[1:], map(int, values[1:])))
-        result[values[0]] = year_data
-
+        if values[0] not in result:
+            result[values[0]] = {}
+        result[values[0]][filiere] = year_data
     return result
 
 def open_csv(path):
@@ -32,39 +26,54 @@ def open_csv(path):
     return data
 
 def get_filiere(path):
-    """Retourne le nom de la filière depuis le chemin du fichier CSV"""
     filename_with_extension = path.split('/')[-1]
     filiere = filename_with_extension.split('.')[0]
     return filiere
 
-def save_json(data, filiere):
-    """Sauvegarde les données dans un fichier JSON"""
-    path = f'data/json/{filiere}.json'
-    os.makedirs(os.path.dirname(path), exist_ok=True)  # Ensure the directory exists
+def merge_json_data(json_data_list):
+    unified_json = {}
+    for json_data in json_data_list:
+        for year, data in json_data.items():
+            if year not in unified_json:
+                unified_json[year] = {}
+            unified_json[year].update(data)
+    return unified_json
+
+def save_json(data, path):
+    os.makedirs(os.path.dirname(path), exist_ok=True)
     with open(path, 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
 
 if __name__ == '__main__':
-    filto_append_allthejson = []
+    json_data_list = []
     path = args.path
     if os.path.isfile(path) and path.endswith(".csv"):
         filiere = get_filiere(path)
         data = open_csv(path)
         json_data = save_to_json(data, filiere)
-        save_json(json_data, filiere)
+        json_data_list.append(json_data)
     elif os.path.isdir(path):
         found_csv = False
         for filename in os.listdir(path):
             if filename.endswith(".csv"):
-                filiere = get_filiere(filename)
                 found_csv = True
                 csv_path = os.path.join(path, filename)
+                filiere = get_filiere(csv_path)
                 data = open_csv(csv_path)
                 json_data = save_to_json(data, filiere)
-                save_json(json_data, filiere)
+                json_data_list.append(json_data)
         if not found_csv:
             print("No CSV files found in the directory.")
             sys.exit(1)
     else:
         print("The provided path is neither a CSV file nor a directory.")
         sys.exit(1)
+
+    # Merge the JSON data and save it
+    unified_json = merge_json_data(json_data_list)
+    unified_json_with_context = {
+        "contexte": "Ce document retrace les statistiques du nombre d'étudiants (sexe, nationalité) inscrits au semestre d'automne depuis 2011 à l'Université de Lausanne.",
+        "data": unified_json
+    }
+    output_name = f"data/json/{args.output_name}"
+    save_json(unified_json_with_context, output_name)
